0c081fbae7ad9863d7d645cd973f39da9f44578b
justiceprime <justicechibundu17@yahoo.com>
2022-10-06 13:18:34 +0100

    whatever

---
+++ index.js
@@ -0,0 +1,31 @@
+const hourEl = document.getElementById("hour")
+const minute = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const am = document.getElementById("ampm")
+
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if (h > 12 ) {
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h
+    m = m < 10 ? "0" + m : m
+    s = s < 10 ? "0" + s : s
+
+
+    hourEl.innerText = h
+    minute.innerText = m
+    seconds.innerText = s
+    am.innerText = ampm
+    setTimeout(()=> {
+        updateClock()
+    }, 1000)
+}
+
+updateClock()
